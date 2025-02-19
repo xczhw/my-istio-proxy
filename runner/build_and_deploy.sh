f813944cf186175ba889afd2ee2dd539484cae6b
xczhw <xczhw@outlook.com>
2025-02-18 19:25:40 -0700

    修改了yaml文件,将imagePullPolicy的设置放到yaml里

--- runner/build_and_deploy.sh
+++ runner/build_and_deploy.sh
@@ -17,7 +17,7 @@ make docker.push TAGS=1.24-dev
 
 # 安装 Istio
 # echo "Installing Istio..."
-# /mydata/istio/istio/out/linux_amd64/istioctl install -f /mydata/istio-testing/work/runner/istio-operator.yaml -y
+/mydata/istio/istio/out/linux_amd64/istioctl install -f /mydata/istio-testing/work/runner/istio-operator.yaml -y
 
 
 # 重新部署 whoami 服务
