04ea62e0bb851d516bc85011eb73c164ba2904c9
xczhw <xczhw@outlook.com>
2025-02-19 19:51:08 -0700

    直接讲envoy拷贝到out目录中,并且每次删除istio_is_init文件来强制重新部署.然后删掉了sleep10 来提速

--- runner/build_and_deploy.sh
+++ runner/build_and_deploy.sh
@@ -8,16 +8,17 @@ docker exec -it istio-proxy-builder bash -c "make build BAZEL_STARTUP_ARGS='' BA
 
 # 复制构建完成的 envoy 二进制文件到 /mydata/istio-testing
 echo "Copying built envoy binary to /mydata/istio-testing..."
-docker cp istio-proxy-builder:/work/bazel-bin/envoy /mydata/istio/istio/envoy
+mkdir -p /mydata/istio/istio/out/
+docker cp istio-proxy-builder:/work/bazel-bin/envoy /mydata/istio/istio/out/envoy
 
 # 进入 Istio 目录并构建 Istio Docker 镜像
 echo "Building Istio Docker image..."
 cd /mydata/istio/istio
+rm -f /mydata/istio/istio/out/istio_is_init
 make docker.push TAGS=1.24-dev
 
 # 安装 Istio
 # echo "Installing Istio..."
-sleep 10
 /mydata/istio/istio/out/linux_amd64/istioctl install -f /mydata/istio-testing/work/runner/istio-operator.yaml -y
 
 
